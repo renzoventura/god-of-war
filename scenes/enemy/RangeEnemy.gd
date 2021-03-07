f57c9b33a7e812e3c3f26981efe7c813974c383e
renzoventura <edrickventura1996@gmail.com>
2021-03-07 15:32:50 +1300

    Sprint 1 - DONE

--- scenes/enemy/RangeEnemy.gd
+++ scenes/enemy/RangeEnemy.gd
@@ -5,8 +5,10 @@ onready var bullet_container = $"Bullets"
 onready var bullet_timer = $"BulletSpawner"
 var canAttack : bool = false
 var bullet_cooldown : bool = false
+var charge_timer_lenght_list = [1,2,3,4]
 
 func _ready():
+	bullet_timer.wait_time = charge_timer_lenght_list[randi() % charge_timer_lenght_list.size()]
 	bullet_timer.start()
 	health = 2
 	maxhealth = 2
@@ -24,6 +26,7 @@ func attack_feature():
 
 func attack_mode_on():
 	if(!canAttack):
+		print(bullet_timer.wait_time)
 		bullet_timer.start()
 
 func fireBullet():
@@ -32,6 +35,8 @@ func fireBullet():
 	var bullet_instance = bullet.instance()
 	var dir = (player.global_position - global_position).normalized()
 	bullet_instance.direction = dir
+	bullet_timer.wait_time = charge_timer_lenght_list[randi() % charge_timer_lenght_list.size()]
+	print(bullet_timer.wait_time)
 	bullet_container.add_child(bullet_instance)
 	bullet_timer.start()
 
