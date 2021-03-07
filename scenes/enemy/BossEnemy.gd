c7cb748ae8315a236309928484e6e204f773311e
renzoventura <edrickventura1996@gmail.com>
2021-03-07 18:31:24 +1300

    Added orb randomizer

--- scenes/enemy/BossEnemy.gd
+++ scenes/enemy/BossEnemy.gd
@@ -3,9 +3,20 @@ extends "res://scenes/enemy/Enemy.gd"
 var player = null
 export var SPEED:int = 30
 onready var weapons = $"Weapons"
+onready var orbsRandomizer = $"OrbsRandomizer"
 var spin_value = 0
 
+var orb_spin_multiplier = 2
+var orb_position_speed = 0.7
+var value = 0.2
+var moved_value = 0
+var max_moved_value = 50
+var max_negative_moved_value = -30
+
+var orb_spin_multiplier_list = [1.5, 2, 2.5]
+
 func _ready():
+	orbsRandomizer.start()
 	health = 100
 	maxhealth = 100
 	pass # Replace with function body.
@@ -30,19 +41,13 @@ var is_adding = false
 
 func get_rotation():
 	if(is_adding):
-		return weapons.rotation_degrees - PI/2;
+		return weapons.rotation_degrees - (PI/orb_spin_multiplier);
 	else:
-		return weapons.rotation_degrees + PI/2
+		return weapons.rotation_degrees + (PI/orb_spin_multiplier);
 		
 func update_rotation(value):
 	if(value > 359): 
 		weapons.rotation_degrees = 0
-		
-var orb_position_speed = 0.7
-var value = 0.2
-var moved_value = 0
-var max_moved_value = 50
-var max_negative_moved_value = -30
 
 func move_orbs():
 	moved_value += value
@@ -77,3 +82,8 @@ func spwan_bashers():
 
 func spawn_giants():
 	print("spawning giants")
+
+
+func _on_OrbsRandomizer_timeout():
+	orb_spin_multiplier = orb_spin_multiplier_list[randi() % orb_spin_multiplier_list.size()]
+	pass # Replace with function body.
