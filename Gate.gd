a0a5d0399eb4965fa9c1dcf530a20093251ba2b0
renzoventura <edrickventura1996@gmail.com>
2021-03-11 02:10:07 +1300

    Finished level 1

--- Gate.gd
+++ Gate.gd
@@ -6,9 +6,3 @@ var closed_preload = preload("res://assets/level/gate_closed.png")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-func _on_Gate_body_entered(body):
-	pass # Replace with function body.
-
-func _on_Gate_area_entered(area):
-	pass # Replace with function body.
---
+++ scenes/level/LEVELS/Level1.gd
@@ -0,0 +1,26 @@
+extends "res://scenes/level/TemplateLevel.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Gate_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_Gate_body_entered(body):
+	if(body.name == "Player"):
+		get_tree().change_scene("res://scenes/level/LEVELS/Level2.tscn")
+	pass # Replace with function body.
